2b65ff0893d82c9035fcaa8080aa4d534982da73
Jani Nurmi <jani.nurmi@edu.turkuamk.fi>
2022-12-07 23:16:55 +0200

    User model lisätty ja yksittäisen pelin sivu myös

--- board_gamer/models.py
+++ board_gamer/models.py
@@ -5,6 +5,16 @@ class Game(models.Model):
     """A game the user wants to interact with"""
     text = models.CharField(max_length=100)
     date_added = models.DateTimeField(auto_now_add=True)
+    is_borrowed = models.BooleanField(default=False)
+    borrower = models.ForeignKey('User', on_delete=models.PROTECT, blank=True, null=True)
 
     def __str__(self):
         return self.text
+
+class User(models.Model):
+    """Page user"""
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
