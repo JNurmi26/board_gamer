61656863e3ccb3e0291d066c4005931de95d4eec
Jani Nurmi <jani.nurmi@edu.turkuamk.fi>
2022-12-08 20:54:33 +0200

    Lisätty pelien omistajat ja suojaus

--- board_gamer/views.py
+++ board_gamer/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 from .models import Game
 from .forms import GameForm
@@ -8,19 +10,23 @@ def index(request):
     """The home page for Board Gamer"""
     return render(request, 'board_gamer/index.html')
 
+@login_required
 def games(request):
     """Show all games"""
     games = Game.objects.order_by('date_added')
     context = {'games': games}
     return render(request, 'board_gamer/games.html', context)
 
+@login_required
 def game(request, game_id):
     """Show a single game"""
     print(game_id)
     game = Game.objects.get(id=game_id)
+    # Make sure the game belongs to the current user.
     context = {'game':game}
     return render(request, 'board_gamer/game.html', context)
 
+@login_required
 def new_game(request):
     """Add a new game"""
     if request.method != 'POST':
@@ -30,16 +36,21 @@ def new_game(request):
         # POST data submitted, process data.
         form = GameForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_game = form.save(commit=False)
+            new_game.owner = request.user
+            new_game.save()
             return redirect('board_gamer:games')
 
     # Display a blank or invalid form.
     context = {'form': form}
     return render(request, 'board_gamer/new_game.html', context)
 
+@login_required
 def edit_game(request, game_id):
     """Edit an existing game."""
     game = Game.objects.get(id=game_id)
+    if game.owner != request.user:
+        raise Http404
 
     if request.method != 'POST':
         # Initial request, pre-fill form with current game info.
