ad812d70dd494ed57a321a9431dae3a77c2420f1
TheKake <kalle.lehtinen@hotmail.fi>
2022-12-14 05:59:58 +0200

    Update forms.py

--- board_gamer/forms.py
+++ board_gamer/forms.py
@@ -13,3 +13,10 @@ class BorrowForm(forms.ModelForm):
     class Meta:
         model = Game
         fields = ['is_borrowed',]
+
+
+class UserForm(forms.ModelForm): 
+    username = forms.CharField(max_length=100, required=True)
+    class Meta: 
+        model = Gamer
+        fields = ['username']
